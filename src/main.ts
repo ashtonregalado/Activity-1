f6280adea45018b9e6b137b91e75746ab4e44f0a
Sean Ashton V. Regalado <seanashton.regalado-23@cpu.edu.ph>
2024-09-01 11:17:32 +0800

    first commit

---
+++ src/main.ts
@@ -0,0 +1,45 @@
+//TS File
+
+function myFunction() {
+  let view = document.getElementById("view");
+  if (!view) return;
+
+  let newTD = document.createElement("li");
+  let inputText = (document.getElementById("myText") as HTMLInputElement).value;
+
+  if (!inputText.trim()) return; 
+
+  var node = document.createTextNode(inputText);
+
+  let doneButton = document.createElement("button");
+  doneButton.textContent = "Done";
+  doneButton.style.marginLeft = "10px";
+  doneButton.style.marginRight = "10px"
+
+  doneButton.addEventListener("click", () => {
+    newTD.style.textDecoration = "line-through";
+    newTD.style.color = "gray";
+    doneButton.disabled = true;
+    
+  });
+  
+  newTD.appendChild(doneButton)
+  newTD.appendChild(node);
+  view.appendChild(newTD);
+
+
+  let removeButton = document.createElement("button");
+  removeButton.textContent = "Remove"
+  removeButton.style.marginLeft = "10px";
+
+  removeButton.addEventListener("click", () => {
+    view.removeChild(newTD)
+  });
+
+  newTD.appendChild(removeButton);
+
+  (document.getElementById("myText") as HTMLInputElement).value = "";
+}
+
+
+(window as any).myFunction = myFunction;
